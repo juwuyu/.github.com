5d050a8ffdc2df5282415075330931c96955b042
juwuyu <451229599@qq.com>
2018-07-16 20:00:58 +0800

    js简历

---
+++ IFE-task/lingjichu/task16d-test/main.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    
+    //元素变量
+    var hello = document.querySelector('.helloButton');
+    var hobbies = document.querySelector('.hobbyButton');
+    var message = this.document.querySelector('.message');
+
+    //按钮的监听器
+    hello.addEventListener('click', sayHello);
+    hobbies.addEventListener('click', myHobbies);
+
+    //响应函数
+    function sayHello() {
+        message.textContent = '';
+        message.style.visibility = 'visible'
+        message.textContent = '很高兴 : ) 能遇见你！';
+        message.style.backgroundColor = 'yellow';
+    }
+    function myHobbies() {
+        message.style.visibility = 'visible'
+        message.textContent = '';
+        var hobbiesArray = ['电影','美食','舞蹈','单机游戏']
+        for(var i = 0; i < hobbiesArray.length; i++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = hobbiesArray[i];
+            message.appendChild(listItem);
+                }
+        message.style.background = '#9966CC' ;
+    }
+
+}
